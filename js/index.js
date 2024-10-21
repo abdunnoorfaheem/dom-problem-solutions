ccf33aa7313f439bd2bc43c4f648d56c8ef68208
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-10-21 07:41:04 +0600

    problem 2 solve

--- js/index.js
+++ js/index.js
@@ -8,3 +8,19 @@ document.getElementById("btnOne").addEventListener("click", function() {
 
 
 });
+
+
+let count = 0;
+document.getElementById("btnTwo").addEventListener("click", function() {
+
+
+    let countNumber = document.getElementById("count");
+    count = count + 1;
+
+
+    let setCount = document.getElementById("count").innerText = count;
+
+
+
+
+})
